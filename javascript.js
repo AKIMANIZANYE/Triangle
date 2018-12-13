77daeb330b27f5b6e266c3595d32cee51bfff24e
Claudine Akimanizanye <akimanizanye.claudine@gmail.com>
2018-12-13 13:29:57 +0200

    I styled my  html code

--- javascript.js
+++ javascript.js
@@ -1,2 +1,27 @@
-var base = parseInt(prompt("Type the width of a Triangle : "));
-var height = parseInt(prompt("Type the height of a Triangle : "));
+function checkinputs()
+{
+
+inp1 = parseInt(document.getElementById.value(one));
+inp2 = parseInt(document.getElementById.value(one));
+inp3 = parseInt(document.getElementById.value(one));
+
+sideA = (inp1 + inp2);
+sideB = (inp1 + inp3);
+sideC = (inp2 + inp3);
+    if (sideA == sideB && sideB == sideC) {
+    alert("Equalateral");
+    }
+   else if (sideA == sideB && sideB !=sideC) {
+    alert("Isosceles");
+    }
+    
+    else if (sideA != sideB  && sideC != sideA) {
+    alert("Scalene!");
+    }
+    else{
+        alert("none of it");
+    }
+}
+    document.getElementById("demo").innerHTML =
+    "Number of forms: " + document.forms.length;
+
