638ea2f0ca99ac2c0567b149b89d19f03a182c7b
Claudine Akimanizanye <akimanizanye.claudine@gmail.com>
2018-12-14 08:15:11 +0200

    I linked javascript

--- javascript.js
+++ javascript.js
@@ -1,27 +1,24 @@
 function checkinputs()
 {
-
-inp1 = parseInt(document.getElementById.value(one));
-inp2 = parseInt(document.getElementById.value(one));
-inp3 = parseInt(document.getElementById.value(one));
-
+/* Var = parseInt(document.Name_Of_Element_Form.Field_Name(Input).value); */
+/* Input Fields */
+inp1 = parseInt(document.Triangle_Inputs.input1.value);
+inp2 = parseInt(document.Triangle_Inputs.input2.value);
+inp3 = parseInt(document.Triangle_Inputs.input3.value);
+/* Side options */
 sideA = (inp1 + inp2);
 sideB = (inp1 + inp3);
 sideC = (inp2 + inp3);
     if (sideA == sideB && sideB == sideC) {
     alert("Equalateral");
     }
-   else if (sideA == sideB && sideB !=sideC) {
+    if (sideA == sideB && != sideC) {
     alert("Isosceles");
     }
-    
-    else if (sideA != sideB  && sideC != sideA) {
+    if (sideA != sideB == sideC) {
+    alert("Isosceles");
+    }
+    if (sideA != sideB != sideC != sideA) {
     alert("Scalene!");
     }
-    else{
-        alert("none of it");
-    }
 }
-    document.getElementById("demo").innerHTML =
-    "Number of forms: " + document.forms.length;
-
